e092e4e957a5c267a09b03e87b2c78e02f612b30
andiaditya21 <andiaditya21@gmail.com>
2020-12-24 16:05:30 +0800

    ganti package version of node-sass dari @5.0.0 ke @4.14.1, menentukan types dari props, logic sederhana untuk inject classname, conditional rendering untuk link external (a) dan link internal (link)

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,5 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 
-export default function Btn(props) {
+const Button = (props) => {
+  const className = props.className;
+
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  // menghandle onClick
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  // rendering option (link external (a) or link internal (link))
+  if (props.type === "link") {
+    // link external (a)
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined} // noopener & noreferrer, optiomasi SEO
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      // link internal (link)
+      <Link
+        to={props.href}
+        className={className.join(" ")}
+        style={props.style}
+        onClick={onClick}
+      >
+        {props.children}
+      </Link>;
+    }
+  }
+
   return <div></div>;
-}
+};
+
+Button.propTypes = {
+  type: propTypes.oneOf(["button", "link"]), // Button hanya menerima 2 property
+  onClick: propTypes.func,
+  className: propTypes.string,
+  href: propTypes.string,
+  target: propTypes.string, // dipakai ketika routing ke external link
+  isExternal: propTypes.bool,
+  isLoading: propTypes.bool,
+  isDisabled: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
+
+export default Button;
